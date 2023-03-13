9b42d35ced5614ac87e177f4e51c67b32b18cda1
Lukas Bombach <lukas.bombach@googlemail.com>
2023-03-13 11:52:06 +0100

    input

--- src/build.ts
+++ src/build.ts
@@ -1,13 +1,34 @@
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
 import chalk from "chalk";
 import { bundle } from "./bundler";
 
-const start = performance.now();
-process.stdout.write("compiling... ");
-process.stdout.write("\x1Bc\n");
+const rl = readline.createInterface({ input, output });
 
-bundle("app", "dist/app").then(() => {
+function clearConsole() {
+  process.stdout.write("\x1Bc\n");
+}
+
+function getDuration(start: number, stop: number) {
+  return Number((stop - start) / 1000).toFixed(3);
+}
+
+async function build() {
+  const start = performance.now();
+  await bundle("app", "dist/app");
   const stop = performance.now();
-  const inSeconds = (stop - start) / 1000;
-  const rounded = Number(inSeconds).toFixed(3);
-  process.stdout.write(`\ndone ${chalk.dim(`${rounded}ms `)}`);
-});
+
+  const duration = getDuration(start, stop);
+  const msg = `\ndone ${chalk.dim(`${duration}ms`)}\n`;
+
+  const input = await rl.question(msg);
+
+  console.log("input was", JSON.stringify(input));
+
+  if (input === "q") {
+    process.exit(0);
+  }
+}
+
+clearConsole();
+build();
