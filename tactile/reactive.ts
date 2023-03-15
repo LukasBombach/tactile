6fa8c4685aeab66a3e080bba10d3dea4bcaf3bc9
Lukas Bombach <lukas.bombach@googlemail.com>
2023-03-15 11:39:05 +0100

    wrote a ractive runtime

--- tactile/reactive.ts
+++ tactile/reactive.ts
@@ -1,1 +1,43 @@
-export function value<T>(initialValue: T): [currentValue: T, setValue: (newValue: T) => void] {}
+type Value<T> = [getValue: () => T, setValue: (newValue: T) => void];
+type Reaction = () => void;
+
+let currentReaction: Reaction | null = null;
+const queue = new Set<Reaction>();
+
+export function value<T>(value: T): Value<T> {
+  const reactions = new Set<Reaction>();
+
+  const getValue = () => {
+    if (currentReaction) reactions.add(currentReaction);
+    return value;
+  };
+
+  const setValue = (newValue: T) => {
+    value = newValue;
+    reactions.forEach(r => queue.add(r));
+
+    // queue.values() returns an iterator over the reactions of the set
+    const values = queue.values();
+
+    let item = values.next();
+
+    // we iterate over the reactions until none is left and run each one
+    // running a reaction can add more reactions to the set, thereby extending the set
+    // while we are iterating over it
+    // 📌 This ends up working through the reaction tree depth-first
+    while (!item.done) {
+      queue.delete(item.value);
+      item.value();
+      item = values.next();
+    }
+  };
+
+  return [getValue, setValue];
+}
+
+export function react(reaction: Reaction): void {
+  const previous = currentReaction;
+  currentReaction = reaction;
+  reaction();
+  currentReaction = previous;
+}
